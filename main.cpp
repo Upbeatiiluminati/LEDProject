a8fd0350f08160ec9a5ffc17ac0b850f46327895
Борис Фроликов < >
2016-12-17 17:54:22 +0300

    file containing main()

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <serial.h>
+#include <QQmlContext>
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QQmlEngine engine;
+
+    Serial seri;
+    engine.rootContext()->setContextProperty("seri",&seri);
+    QQmlComponent component(&engine,QUrl(QStringLiteral("qrc:/main.qml")));
+    component.create();
+    return app.exec();
+}
